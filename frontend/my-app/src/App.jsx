0b34543fc260e2a2e3d694c6587ba1d0cbadb592
shreyam2004gupta <shreyamgupta437@gmail.com>
2025-10-16 00:15:09 +0530

    i have updated the frontend

---
+++ frontend/my-app/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"; 
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button variant="solid">Hello ShadCN!</Button>
+    </div>
+  )
+}
