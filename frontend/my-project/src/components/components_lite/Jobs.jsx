7697865e0d86df3645c4d170b65e03c1445cf9f4
shreyam2004gupta <shreyamgupta437@gmail.com>
2026-01-06 02:13:20 +0530

    filter

--- frontend/my-project/src/components/components_lite/Jobs.jsx
+++ frontend/my-project/src/components/components_lite/Jobs.jsx
@@ -6,26 +6,24 @@ import { useSelector } from "react-redux";
 import { motion } from "framer-motion";
 
 const Jobs = () => {
-  const { allJobs , searchQuery } = useSelector((store) => store.job);
-  const [filterJobs,setFilterJobs]=useState(allJobs);
-  useEffect(()=>{
-  if(!searchQuery || searchQuery.trim()===""){
-    setFilterJobs(allJobs);
-    return;
-  }
-    const filterJobs = allJobs.filter((job)=>{
+  const { allJobs, searchedQuery } = useSelector((store) => store.job);
+  const [filterJobs, setFilterJobs] = useState(allJobs);
+  useEffect(() => {
+    if (!searchedQuery || searchedQuery.trim() === "") {
+      setFilterJobs(allJobs);
+      return;
+    }
+    const filterJobs = allJobs.filter((job) => {
       return (
-        job.title?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        job.description?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        job.location
-          ?.toLowerCase()
-          .includes(searchQuery.toLowerCase()) ||
-          job.experience?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          job.salary?.toLowerCase().includes(searchQuery.toLowerCase()) 
+        job.title?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+        job.description?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+        job.location?.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+        String(job.experienceLevel || '').toLowerCase().includes(searchedQuery.toLowerCase()) ||
+        String(job.salary || '').toLowerCase().includes(searchedQuery.toLowerCase())
       );
     });
     setFilterJobs(filterJobs);
-  },[allJobs,searchQuery])
+  }, [allJobs, searchedQuery]);
   return (
     <div>
       <Navbar />
@@ -41,12 +39,13 @@ const Jobs = () => {
             <div className="flex-1 overflow-y-auto pb-5">
               <div className="grid grid-cols-3 gap-4">
                 {filterJobs.map((job) => (
-                  <motion.div 
-                  initial={{opacity:0,x:100}}
-                  animate={{opacity:1,x:0}}
-                  exit={{opacity:0,x:-100}}
-                  transition={{duration:0.4}}
-                  key={job.id}>
+                  <motion.div
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 0.4 }}
+                    key={job._id}
+                  >
                     <Job9 job={job} />
                   </motion.div>
                 ))}
