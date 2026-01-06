90520000c77743fcd9ea4ec6849f9c4496009185
shreyam2004gupta <shreyamgupta437@gmail.com>
2026-01-06 17:57:48 +0530

    category

--- frontend/my-project/src/components/components_lite/Category.jsx
+++ frontend/my-project/src/components/components_lite/Category.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Carousel,
   CarouselContent,
@@ -6,51 +6,52 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "../ui/carousel";
-import {Button} from "../ui/button"
-import { setSearchQuery } from '@/Redux/jobSlice';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { Button } from "../ui/button";
+import { setSearchQuery } from "@/Redux/jobSlice";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-
-const Categories= [
-      "Frontend-developer",
-      "Backend-developer",
-      "AI-ML Enginer",
-      "Cloud-Computing",
-      "Full-Stack-Developer",
-      "Data-Scientist",
-      "Data-Analyast",
-      "Prompt-Enginere",
-  ]
-  const Category = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const searchjobHandler = (query) => {
-      dispatch(setSearchQuery(query));
-      navigate("/Browser");
-    };
+const Categories = [
+  "Frontend-developer",
+  "Backend-developer",
+  "AI-ML Enginer",
+  "Cloud-Computing",
+  "Full-Stack-Developer",
+  "Data-Scientist",
+  "Data-Analyast",
+  "Prompt-Enginere",
+];
+const Category = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const searchjobHandler = (query) => {
+    dispatch(setSearchQuery(query));
+    navigate("/Browser");
+  };
   return (
+    <div>
       <div>
-        <div>
-            <h1 className="text-2xl font-bold text-center text-blue-500 p-5">Categories of our Job</h1>
-         </div>
+        <h1 className="text-2xl font-bold text-center text-blue-500 p-5">
+          Categories of our Job
+        </h1>
+      </div>
       <Carousel className="w-full max-w-xl mx-auto">
         <CarouselContent>
-            {
-                Categories.map((categories,index)=>{
-                    return(
-                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-                        <Button onClick={()=>searchjobHandler(categories)}>{categories}</Button>
-                        </CarouselItem>
-                        );
-                })
-            }
-            </CarouselContent>
-            <CarouselPrevious/>
-            <CarouselNext/>
+          {Categories.map((categories, index) => {
+            return (
+              <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                <Button onClick={() => searchjobHandler(categories)}>
+                  {categories}
+                </Button>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
       </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- frontend/my-project/src/components/components_lite/Header.jsx
+++ frontend/my-project/src/components/components_lite/Header.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from 'react'
-import { Button} from "../ui/button";
-import {Search} from "lucide-react";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import { Search } from "lucide-react";
 import { HiBuildingOffice2 } from "react-icons/hi2";
-import { useDispatch } from 'react-redux';
-import { setSearchQuery } from '@/Redux/jobSlice';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { setSearchQuery } from "@/Redux/jobSlice";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const [query,setQuery]=useState("");
+  const [query, setQuery] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const searchjobHandler =()=>{
+  const searchjobHandler = () => {
     dispatch(setSearchQuery(query));
-    navigate("/Browser");
+    navigate("/Browse");
   };
   return (
     <div className="gap-5">
       <div className="text-center">
         <div className="gap-4 flex-col flex">
           <span className="px-4 py-2 rounded-full flex justify-center items-center bg-gray-200 text-red-500">
-            <span ><HiBuildingOffice2 /></span>Get Your Dream Job
+            <span>
+              <HiBuildingOffice2 />
+            </span>
+            Get Your Dream Job
           </span>
 
           <h2 className="text-6xl">
             Search Your <span className="text-blue-400">Job !</span>
           </h2>
         </div>
-         <p>Starting hunting your job</p>
-         <div className="flex w-[30%] shadow-lg border border-gray-300 pl-3 rounded-full items-center gap-4 mx-auto">
-            <input text="text"
-            onChange={(e)=>setQuery(e.target.value)} placeholder="Find your Drean Job" className="outline-none border-none w-full"/>
-            <Button onClick={searchjobHandler} className ="rounded-r-full" >
-            <Search className="h-6 w-6"/>
-            </Button>
-         </div>
+        <p>Starting hunting your job</p>
+        <div className="flex w-[30%] shadow-lg border border-gray-300 pl-3 rounded-full items-center gap-4 mx-auto">
+          <input
+            text="text"
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Find your Drean Job"
+            className="outline-none border-none w-full"
+          />
+          <Button onClick={searchjobHandler} className="rounded-r-full">
+            <Search className="h-6 w-6" />
+          </Button>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
